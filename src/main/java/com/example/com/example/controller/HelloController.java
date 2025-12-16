7a66eaa785b83777feb41566e4565ba917795522
sudhakar gutti <sudhakar8324@gmail.com>
2025-12-16 19:48:23 +0530

    hello changes

--- src/main/java/com/example/com/example/controller/HelloController.java
+++ src/main/java/com/example/com/example/controller/HelloController.java
@@ -2,12 +2,14 @@ package com.example.com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/hello")
 public class HelloController {
 
-    @GetMapping("/api/hello/{message}")
+    @GetMapping("/{message}")
     public String hello(@PathVariable String message){
         return message;
     }
