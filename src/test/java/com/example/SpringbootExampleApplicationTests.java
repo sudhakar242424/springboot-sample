2212e05166800bce976f41843588766afbcad81e
sudhakar gutti <sudhakar8324@gmail.com>
2025-12-17 00:18:25 +0530

    hello changes

--- src/test/java/com/example/SpringbootExampleApplicationTests.java
+++ src/test/java/com/example/SpringbootExampleApplicationTests.java
@@ -1,11 +1,32 @@
 package com.example;
 
+import com.example.controller.HelloController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringbootExampleApplicationTests {
 
+    @InjectMocks
+    private HelloController helloController;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this); // Initialize mocks
+    }
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        String message = "sudha";
+        String response = helloController.welcome(message);
+
+        // Assert
+        assertEquals("sudha, Welcome to SudhaTech..!!", response);
+    }
 
 }
