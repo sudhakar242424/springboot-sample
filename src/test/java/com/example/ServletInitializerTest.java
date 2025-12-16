1708ae610dd2173ba0e1e30925f287fc1326ba04
sudhakar gutti <sudhakar8324@gmail.com>
2025-12-17 00:33:52 +0530

    hello changes

--- src/test/java/com/example/ServletInitializerTest.java
+++ src/test/java/com/example/ServletInitializerTest.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ServletInitializerTest {
+class ServletInitializerTest {
 
     @InjectMocks
     ServletInitializer servletInitializer;
